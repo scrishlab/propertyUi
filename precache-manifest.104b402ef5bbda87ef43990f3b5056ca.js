65f3ed6b69f1d98b423db4eed39b9e39e2f4db6c
Thomas Whalen-Bridge <thomasjmwb@gmail.com>
2021-09-26 16:57:37 +0100

    Updates

---
+++ precache-manifest.104b402ef5bbda87ef43990f3b5056ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "592671bd90c230a80f65",
+    "url": "/property-ui/static/css/main.d3345f6b.chunk.css"
+  },
+  {
+    "revision": "592671bd90c230a80f65",
+    "url": "/property-ui/static/js/main.99e56431.chunk.js"
+  },
+  {
+    "revision": "8afdd941be35762489c1",
+    "url": "/property-ui/static/js/runtime~main.d58f0b57.js"
+  },
+  {
+    "revision": "66045548b14d3def6768",
+    "url": "/property-ui/static/js/2.d936fc6c.chunk.js"
+  },
+  {
+    "revision": "03536cb0a75ce125b824",
+    "url": "/property-ui/static/js/3.d0e5de46.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/property-ui/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "dc9967425eb0fd35773ab7dc2797fc37",
+    "url": "/property-ui/index.html"
+  }
+];
