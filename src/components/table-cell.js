4eaab95723cd36584c53b538f6840a9936fc2440
Thomas Whalen-Bridge <thomasjmwb@gmail.com>
2021-09-26 22:12:44 +0100

    Sorting and filtering

--- src/components/table-cell.js
+++ src/components/table-cell.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 export default function TableCell(props){
     const {colHeader, row} = props;
+    const AddressRow = ()=>(<a href={row.url}>{row[colHeader.accessor]}</a>);
+    const DefaultRow = ()=>(<p>{row[colHeader.accessor]}</p>);
+    // const ImageRow = ()=>row[colHeader.accessor].length>0 ? (<img style={{height:"80px"}} src={row[colHeader.accessor][0]}/>) : "";
+    let component = DefaultRow;
+    if(colHeader.accessor=="address") component=AddressRow;
+    // if(colHeader.accessor=="pictures") component=ImageRow;
     return (
         <td>
-            {colHeader.accessor=="address" ? 
-                (<a href={row.url}>{row[colHeader.accessor]}</a>) :
-                (<p>{row[colHeader.accessor]}</p>)
-            }
+            {component()}
             
         </td>
     )
--- src/components/table.js
+++ src/components/table.js
@@ -1,5 +1,5 @@
 
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import usePropertyProfiles from "../hooks/use-property-profiles";
 import TableCell from "./table-cell";
 const columns = [
@@ -15,26 +15,52 @@ const columns = [
       Header: "Price",
       accessor: "price",
     },
+    // {
+    //     Header: "Image",
+    //     accessor: "pictures"
+    // }
   ];
 
   
-
+const PAGE_SIZE=10;
 export default function table(){
     const [profiles, isLoading, error, getProfiles] = usePropertyProfiles([]);
-      useEffect(() => {
+    const [sortProperty, setSortProperty] = useState("acres");
+    const [sortDirection, setSortDirection] = useState(-1);//-1 is descending
+    const [pageNumber, setPageNumber] = useState(1);
+    const [filteredProfiles, setFilteredProfiles] = useState([]);
+    useEffect(()=>{
+        //sort profiles, then setFilteredProfiles
+        const sortFn = (first, second)=>{
+            if(first[sortProperty]==second[sortProperty])return 0;
+            if(!first[sortProperty])return 1;
+            if(!second[sortProperty])return -1;
+            return first[sortProperty] > second[sortProperty] ? 1*sortDirection : -1*sortDirection;
+        };
+        // const paged = sorted.slice(0, pageNumber*PAGE_SIZE);
+        const filtered = profiles.filter((p,i)=>profiles.indexOf(profiles.find(pr=>pr.url===p.url))===i); //dedupe
+        const sorted = filtered.sort(sortFn);
+        setFilteredProfiles(sorted);
+    }, [profiles, sortProperty, sortDirection, pageNumber]);
+    useEffect(() => {
         getProfiles();
-      }, []);
+    }, []);
+    function sortClick(){
+        if(sortProperty!=this)return setSortProperty(this) && setSortDirection(-1);
+        setSortDirection(sortDirection*-1);
+    }
     return (
         <table>
+        {isLoading ? (<h1>Loading..</h1>) : ""}
       <thead>
           <tr>
             {columns.map((column, j) => (
-              <th key={j}>{column["Header"]}</th>
+              <th key={j} onClick={sortClick.bind(column.accessor)}>{column["Header"]}</th>
             ))}
           </tr>
       </thead>
       <tbody>
-        {profiles.map((row, i) => {
+        {filteredProfiles.map((row, i) => {
             return (
                 <tr key={i}>
                     {columns.map((colHeader, k)=><TableCell key={k} colHeader={colHeader} row={row}></TableCell>)}
